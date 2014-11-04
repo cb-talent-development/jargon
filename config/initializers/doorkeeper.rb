1f58d4af1d15e9f8a6f609c3523f2174e0085e30
Colin Ewen <colin@draecas.com>
2014-11-04 15:45:42 -0500

    Doorkeeper views

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -5,12 +5,9 @@ Doorkeeper.configure do
     current_user || warden.authenticate!(:scope => :user)
   end
 
-  # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
-  # admin_authenticator do
-  #   # Put your admin authentication logic here.
-  #   # Example implementation:
-  #   Admin.find_by_id(session[:admin_id]) || redirect_to(new_admin_session_url)
-  # end
+  admin_authenticator do
+    current_user || warden.authenticate!(:scope => :user)
+  end
 
   # Provide support for an owner to be assigned to each registered application (disabled by default)
   # Optional parameter :confirmation => true (default false) if you want to enforce ownership of
